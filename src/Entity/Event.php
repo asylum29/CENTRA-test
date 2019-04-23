9f4acdd7e20319d9bb8e46c7ee9e4c8fd35b26f9
asylum29 <ksenon3@mail.ru>
2019-04-23 16:37:00 +0700

    fix

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -54,7 +54,7 @@ class Event
     private $imageFile;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $imageName;
 
